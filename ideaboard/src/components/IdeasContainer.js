5c45227ee30149182c96c07cd6c90fc5cf48f153
grace-mugoiri <gracemugoiri@gmail.com>
2020-06-15 17:06:07 +0300

    add state to map all ideas on UI

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -12,14 +12,21 @@ class IdeasContainer extends Component {
 		axios.get('http://localhost:3001/api/v1/ideas.json')
 			.then(response => {
 				console.log(response)
-				this.setState({ ideas: response.data })
+				this.setState({ideas: response.data})
 			})
 			.catch(error => console.log(error))
 	}
 	render() {
 		return (
 			<div>
-				Ideas
+				{this.state.ideas.map((idea) => {
+					return (
+						<div className="tile" key={idea.id}>
+							<h4>{idea.title}</h4>
+							<p>{idea.body}</p>
+						</div>
+					)
+				})}
 			</div>
 		)
 	}
