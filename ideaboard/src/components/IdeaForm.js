8a35bd7c99775fde6860e9e90798c9df285f21f1
grace-mugoiri <gracemugoiri@gmail.com>
2020-06-29 12:49:26 +0300

    add hanlde input on ideaform

--- ideaboard/src/components/IdeaForm.js
+++ ideaboard/src/components/IdeaForm.js
@@ -5,16 +5,21 @@ class IdeaForm extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-
+			title: this.props.idea.title,
+			body: this.props.idea.body
 		}
+	}
+
+	handleInput = (e) => {
+		this.setState({[e.target.name]: e.target.value})
 	}
 
 	render() {
 		return (
 			<div className="tile">
 				<form>
-					<input type="text" className="input" name="title" placeholder="Enter a Title" />
-					<textarea name="body" className="input" placeholder="Desrcibe your idea"></textarea>
+					<input type="text" className="input" name="title" placeholder="Enter a Title" onChange={this.handleInput} />
+					<textarea name="body" className="input" placeholder="Desrcibe your idea" onChange={this.handleInput} ></textarea>
 				</form>
 			</div>
 		);
